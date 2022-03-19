0c2afe05bca76aa023170f8594d08e9d4b3dbbee
coaelecsoft <coaelecsoft@outlook.com>
2022-03-19 16:29:07 +0100

    add new rep

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+
+
+
+app.get('/', (req, res) => {
+    res.send('My node.js app')
+})
+
+   
+
+app.listen(3030)
+
+/*
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
+ 
+app.set('view engine','ejs')
+const basicPages = ['about', 'contact', 'index']
+
+app.get('/', (req, res) => {
+    res.render('main/index')
+})
+
+const usersRouter = require('./routes/users')
+app.use('/users', usersRouter) 
+
+const webRouter = require('./routes/web')
+app.use("/web", webRouter)
+ 
+
+
+
+app.get('/:page', (req, res) => {
+    if (basicPages.includes(req.params.page)) {
+        res.render(`main/${req.params.page}`)
+    } else {
+        res.render(`main/error`)
+    }
+})
+*/

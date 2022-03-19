bcec8d8a954771d947a2eb3dface96560dab5173
coaelecsoft <coaelecsoft@outlook.com>
2022-03-19 17:15:49 +0100

    last commit

--- app.js
+++ app.js
@@ -3,11 +3,32 @@ const app = express()
 
 
 
+
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
+
+app.set('view engine', 'ejs')
+const basicPages = ['about', 'contact', 'index']
+
 app.get('/', (req, res) => {
-    res.send('My node.js app')
+    res.render('main/index')
 })
-app.get('/13', (req, res) => {
-    res.send('ovo je broj 13')
+
+const usersRouter = require('./routes/users')
+app.use('/users', usersRouter)
+
+const webRouter = require('./routes/web')
+app.use("/web", webRouter) 
+
+
+app.get('/:page', (req, res) => {
+    if (basicPages.includes(req.params.page)) {
+        res.render(`main/${req.params.page}`)
+    } else {
+        res.render(`main/error`)
+    }
 })
 
 const port = process.env.PORT || '8080'
